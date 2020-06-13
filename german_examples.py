e3b86f64efebf9fb8bb855683adfa4a910fa0bb2
Dmitry Glyuz <deceivedxchild@gmail.com>
2020-06-13 21:47:14 +0300

    clean code and improve second list printing

--- german_examples.py
+++ german_examples.py
@@ -2,8 +2,6 @@ import random
 import interface
 from german_numbers import int_to_german
 
-second_list = []
-
 print('1 - plus/minus examples\n'
       '2 - multiplizieren/geteilt examples\n'
       '3 - everything\n')
@@ -41,9 +39,9 @@ def print_examples():
         if operation == '/':
             (x, z) = (z, x)
 
-    result = f'{x} {operation} {y} = {z}'
-    second_list.append(int_to_german(x) + ' ' + operation_german + ' ' + int_to_german(y) + ' gleich ' + int_to_german(z))
-    return result, second_list
+    example_numbers = f'{x} {operation} {y} = {z}'
+    rexample_words = int_to_german(x) + ' ' + operation_german + ' ' + int_to_german(y) + ' gleich ' + int_to_german(z)
+    return example_numbers, rexample_words
 
 
 interface.generator(print_examples, 'examples', True)
--- interface.py
+++ interface.py
@@ -9,20 +9,15 @@ def input_int(prompt, min_n, max_n):
     return number
 
 
-def generator(func, things, show_second_list=False, min_num=1, max_num=30, second_list_header='', second_func=None):
-    num_of_outputs = 0
+def generator(func, things, show_second_list=False, min_num=1, max_num=30, second_list_header=''):
+    second_list = [] if show_second_list is True else None
     num_of_outputs = input_int(f'Enter the number of {things} (from {min_num} to {max_num}): ', min_num, max_num)
     for i in range(1, num_of_outputs + 1):
         print(f'{i}. {func()[0]}')
+        if show_second_list is True:
+            second_list.append(func()[1])
     if show_second_list is True:
         print(second_list_header)
-        for i in range(0, num_of_outputs):
-            print(f'{i + 1}. {func()[1][i]}')
+        for line in second_list:
+            print(f'{second_list.index(line) + 1}. {line}')
 
-
-
-def test_func():
-    print('test')
-
-
-# generator('shit', 1, 999, test_func)
